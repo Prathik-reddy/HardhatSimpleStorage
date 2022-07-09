fe4b78bb1d4295c8e3c613db95633f6b392a0fdc
Prathik Reddy <tvkp333555@gmail.com>
2022-07-09 16:13:25 +0530

    verifying the deployed contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,38 @@
 // imports
-const {ethers} = require('hardhat');
-//async function
+const { ethers, run } = require('hardhat');
 
+//async function
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
   console.log("deploying contract .......");
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`deployed contract to : ${simpleStorage.address}`);
+  if(network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY){
+    await simpleStorage.deployTransaction.wait(6);// wait for 6 blocks before verificaiton
+    await verify(simpleStorage.address,[]);
+  }
 }
 
+// verify the contract that is deployed
+async function verify(contactAddress, args) {
+  console.log("verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contactAddress,
+      constructorArguments: args,
+    })
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified")
+    }
+    else {
+      console.log(error);
+    }
+  }
+
+
+}
 main()
   .then(() => process.exit(0))
   .catch((error) => {
